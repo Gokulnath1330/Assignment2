1edf0a1a6734f20538dc4045109eb6ea73a4feb6
Gokulnath1330 <117091752+Gokulnath1330@users.noreply.github.com>
2022-12-08 16:19:58 +0000

    Add files via upload

--- assignment 2.py
+++ assignment 2.py
@@ -30,14 +30,14 @@ def line_plot2(filename):
     df=pd.read_csv(filename,skiprows=4)
     print(df)
     country1 = df.iloc[43,54:59]
-    country2 = df.iloc[61,54:59]
-    country3 = df.iloc[97,54:59]
+    country2 = df.iloc[64,54:59]
+    country3 = df.iloc[6,54:59]
     country4 = df.iloc[115,54:59]
     print(country1,country2,country3,country4)
     plt.figure()
     plt.plot(country1,label='Switzerland',dashes=[2,1])
-    plt.plot(country2,label='Germany',dashes=[2,1])
-    plt.plot(country3,label='Greenland',dashes=[1,4])
+    plt.plot(country2,label='Denmark',dashes=[2,1])
+    plt.plot(country3,label='South Africa',dashes=[1,4])
     plt.plot(country4,label='India',dashes=[2,5])
     plt.xlabel('Year')
     plt.ylabel('% of land area') 
@@ -46,4 +46,21 @@ def line_plot2(filename):
     plt.show()
 line_plot(r"E:\Co2 emission\Co2 emission1.csv")
 line_plot2(r"E:\Forest area\Forest area.csv")
+def Bar_plot(filename):
+    df2=pd.read_csv(filename,skiprows=4)
+    df2=df2.drop(["Country Code","Indicator Name","Indicator Code"],axis=1)
+    print(df2)
+    mnc1=df2.fillna(0)
+    print(mnc1)
+    mnc2=(mnc1.apply(lambda row: row[mnc1["Country Name"].
+                                     isin(['Switzerland','Denmark','Greenland','India'])]))
+    print(mnc2)
+    plt.figure()
+    mnc2.plot(x="Country Name",y=["2010","2011","2012","2013","2015"],kind="bar")
+    plt.xticks(rotation = 0)
+    plt.ylabel('Land area')
+    plt.title("Forest")
+    plt.legend(loc='upper center',bbox_to_anchor=(1.40, 0.6))
+    plt.show()
+Bar_plot(r"E:\Population growth\Population growth.csv")    
     
