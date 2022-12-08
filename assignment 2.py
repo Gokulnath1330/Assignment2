7a39880fe9e92520a26d1fee4f2ab41cc123a032
Gokulnath1330 <117091752+Gokulnath1330@users.noreply.github.com>
2022-12-08 15:46:42 +0000

    Add files via upload

--- assignment 2.py
+++ assignment 2.py
@@ -26,5 +26,24 @@ def line_plot(filename):
     plt.title('Co2 Emission')
     plt.legend(loc='center left',bbox_to_anchor=(1.40, 0.7))
     plt.show()
-
+def line_plot2(filename):
+    df=pd.read_csv(filename,skiprows=4)
+    print(df)
+    country1 = df.iloc[43,54:59]
+    country2 = df.iloc[61,54:59]
+    country3 = df.iloc[97,54:59]
+    country4 = df.iloc[115,54:59]
+    print(country1,country2,country3,country4)
+    plt.figure()
+    plt.plot(country1,label='Switzerland',dashes=[2,1])
+    plt.plot(country2,label='Germany',dashes=[2,1])
+    plt.plot(country3,label='Greenland',dashes=[1,4])
+    plt.plot(country4,label='India',dashes=[2,5])
+    plt.xlabel('Year')
+    plt.ylabel('% of land area') 
+    plt.title('Forest area (% of land area')
+    plt.legend(loc='center right',bbox_to_anchor=(1.40, 0.6))
+    plt.show()
+line_plot(r"E:\Co2 emission\Co2 emission1.csv")
+line_plot2(r"E:\Forest area\Forest area.csv")
     
